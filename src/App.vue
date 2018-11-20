01133cc6039bdf17335b56e1ef33e74a8bd753c8
mamchurrr <s.mamchurrr@gmail.com>
2018-11-20 23:02:18 +0200

    add animation to app

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,10 @@
     <div class="container">
         <app-header></app-header>
         <div class="row wrapper">
-            <div class="col-12">
-                <router-view/>
+             <div class="col-12">
+                <transition name="slide" mode="out-in">
+                    <router-view/>
+                </transition>
             </div>
         </div>
     </div>
@@ -33,6 +35,35 @@
         margin-top: 15px;
     }
 
+    .slide-enter-active {
+        animation: slide-in 200ms ease-out forwards;
+    }
+
+    .slide-leave-active {
+        animation: slide-out 200ms ease-out forwards;
+    }
+    
+    @keyframes slide-in {
+        from {
+            transform: translateY(-30px);
+            opacity: 0;
+        }
+        to {
+            transform: translateY(0);
+            opacity: 1;
+        }
+    }
+    
+    @keyframes slide-out {
+        from {
+            transform: translateY(0);
+            opacity: 1;
+        }
+        to {
+            transform: translateY(-30px);
+            opacity: 0;
+        }
+    }
         
 </style>
 
